1a7723ca63852a112c5f784286bf43bf2ccf579e
CodePhantom01 <rohit.pratap.singh.engg@gmail.com>
2026-01-06 21:07:20 +0530

    Fix CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,27 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const wordRoutes = require('./routes/wordRoutes');
-const userRoutes = require('./routes/userRoutes');
+const wordRoutes = require("./routes/wordRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://lexi-sarthi.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-app.use('/api/words', wordRoutes);
+app.use("/api/words", wordRoutes);
+app.use("/api/users", userRoutes);
 
-app.get('/', (req, res) => {
-  res.send('LexiSarthi API is running');
+app.get("/", (req, res) => {
+  res.send("LexiSarthi API is running");
 });
-
-app.use('/api/users', userRoutes);
 
 module.exports = app;
